d2fcb1776e2058ec9e9a8f913302428ddc7aec62
Kapil Taspa <kapiltaspa@gmail.com>
2023-06-17 20:30:49 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class MedicalRiskCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,27 +19,47 @@ public class main {
         System.out.print("Weight (in kg): ");
         double weight = scanner.nextDouble();
 
+        System.out.print("Do you smoke? (Y/N): ");
+        String smokeInput = scanner.next();
+        boolean smoke = smokeInput.equalsIgnoreCase("Y");
+
+        System.out.print("Do you have a family history of heart disease? (Y/N): ");
+        String familyHistoryInput = scanner.next();
+        boolean familyHistory = familyHistoryInput.equalsIgnoreCase("Y");
+
         // Calculate BMI
-        double bmi = weight / ((height / 100) * (height / 100));
+        double bmi = calculateBMI(height, weight);
 
         // Calculate risk based on input factors
-        String disease = calculateRisk(age, gender, bmi);
+        String disease = calculateRisk(age, gender, bmi, smoke, familyHistory);
 
         System.out.println("Based on the provided information, you may be at risk of: " + disease);
 
         scanner.close();
     }
 
-    public static String calculateRisk(int age, String gender, double bmi) {
+    public static double calculateBMI(double height, double weight) {
+        return weight / ((height / 100) * (height / 100));
+    }
+
+    public static String calculateRisk(int age, String gender, double bmi, boolean smoke, boolean familyHistory) {
         String disease = "";
 
         // Risk assessment algorithm
-        if (age > 40 && gender.equalsIgnoreCase("M") && bmi >= 30) {
+        if (age > 40 && gender.equalsIgnoreCase("M") && bmi >= 30 && smoke && familyHistory) {
             disease = "Heart Disease";
-        } else if (age > 50 && gender.equalsIgnoreCase("F") && bmi >= 25) {
+        } else if (age > 50 && gender.equalsIgnoreCase("F") && bmi >= 25 && smoke && familyHistory) {
             disease = "Type 2 Diabetes";
-        } else if (age > 60 && gender.equalsIgnoreCase("M") && bmi >= 28) {
+        } else if (age > 60 && gender.equalsIgnoreCase("M") && bmi >= 28 && smoke && familyHistory) {
             disease = "Hypertension";
+        } else if (age > 45 && gender.equalsIgnoreCase("F") && bmi >= 28 && smoke && familyHistory) {
+            disease = "Breast Cancer";
+        } else if (age > 55 && gender.equalsIgnoreCase("M") && bmi >= 28 && smoke && familyHistory) {
+            disease = "Prostate Cancer";
+        } else if (age > 40 && gender.equalsIgnoreCase("M") && bmi >= 25 && smoke && !familyHistory) {
+            disease = "Lung Cancer";
+        } else if (age > 30 && gender.equalsIgnoreCase("F") && bmi >= 25 && smoke && !familyHistory) {
+            disease = "Cervical Cancer";
         } else {
             disease = "No significant risk identified";
         }
